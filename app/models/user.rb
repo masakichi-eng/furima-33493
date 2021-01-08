bed7a18decd8bf9795ef757510ab8f4bde97add0
masakichi-eng <makisankonan@gmail.com>
2021-01-08 12:26:55 +0900

    バリデーションマイグレーションファイル作成２

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :nickname, presence: true,
+         validates :nickname, presence: true
          validates :sei_kanji, presence: true, format: { with: /\A[ぁ-ん一-龥々]+\z/,message:'漢字を入力してください' }
          validates :mei_kanji, presence: true, format: { with: /\A[ぁ-ん一-龥々]+\z/,message:'漢字を入力してください' }
          validates :sei_katakana, presence: true, format: { with: /\A[ァ-ヶ]+\z/,message:'カナを入力してください' }
          validates :mei_katakana, presence: true, format: { with: /\A[ァ-ヶ]+\z/,message:'カナを入力してください' }
          validates :birthday,presence: true
-        
 end
