08185c5096df5e0b4bc11fb42bcf14adb258e6c5
masakichi-eng <makisankonan@gmail.com>
2021-01-09 12:49:40 +0900

    withオプション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :sei_kanji, presence: true, format: { with: /\A[ぁ-ん一-龥々]+\z/, message: '漢字を入力してください' }
-  validates :mei_kanji, presence: true, format: { with: /\A[ぁ-ん一-龥々]+\z/, message: '漢字を入力してください' }
-  validates :sei_katakana, presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: 'カナを入力してください' }
-  validates :mei_katakana, presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: 'カナを入力してください' }
-  validates :birthday, presence: true
+with_options presence: true do
+  validates :nickname
+  validates :sei_kanji, format: { with: /\A[ぁ-ん一-龥々]+\z/, message: '漢字を入力してください' }
+  validates :mei_kanji, format: { with: /\A[ぁ-ん一-龥々]+\z/, message: '漢字を入力してください' }
+  validates :sei_katakana, format: { with: /\A[ァ-ヶ]+\z/, message: 'カナを入力してください' }
+  validates :mei_katakana, format: { with: /\A[ァ-ヶ]+\z/, message: 'カナを入力してください' }
+  validates :birthday
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: '半角で入力してください' }
-  
+end
 end
