2191d54d3b5368158f088ec20b55503593fb13ed
masakichi-eng <makisankonan@gmail.com>
2021-01-12 11:22:11 +0900

    同じ記述削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,7 +35,7 @@ class ItemsController < ApplicationController
 
   def move_to_index
     @item = Item.find(params[:id])
-    unless user_signed_in? && current_user.id == @item.user.id
+    unless current_user.id == @item.user.id
       redirect_to action: :index
     end
   end
