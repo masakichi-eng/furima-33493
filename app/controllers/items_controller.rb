491d93911873ac22f7ac4ecf87993c87d4148aff
masakichi-eng <makisankonan@gmail.com>
2021-01-12 11:18:46 +0900

    同じ処理まとめ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new,:edit]
   before_action :move_to_index, only: [:edit]
+  before_action :find_item, only: [:show,:edit,:update]
   def index
     @items = Item.order("created_at DESC")
   end
@@ -10,15 +11,15 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+    
   end
 
   def edit
-    @item = Item.find(params[:id])
+   
   end
 
   def update
-    @item = Item.find(params[:id])
+    
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -37,9 +38,13 @@ class ItemsController < ApplicationController
 
   def move_to_index
     @item = Item.find(params[:id])
-    unless user_signed_in? && current_user.id == @item.user.id
+    unless current_user.id == @item.user.id
       redirect_to action: :index
     end
+  end
+
+  def find_item
+    @item = Item.find(params[:id])
   end
 
   private
