a3567f36aacafe13bbe6101081e2d768c0c6a680
masakichi-eng <makisankonan@gmail.com>
2021-01-12 11:33:56 +0900

    moveto-index修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new,:edit]
-  before_action :move_to_index, only: [:edit]
   before_action :find_item, only: [:show,:edit,:update]
+  before_action :move_to_index, only: [:edit]
+  
   def index
     @items = Item.order("created_at DESC")
   end
@@ -37,7 +38,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    @item = Item.find(params[:id])
+    
     unless current_user.id == @item.user.id
       redirect_to action: :index
     end
