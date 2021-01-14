d93f6c64dd0e6ee56b8ab50f0755230abaa9b562
masakichi-eng <makisankonan@gmail.com>
2021-01-14 20:16:02 +0900

    編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,12 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up,
                                       keys: [:nickname, :sei_kanji, :mei_kanji, :sei_katakana, :mei_katakana, :birthday])
   end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
+
+
 end
